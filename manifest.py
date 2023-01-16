9b5558b6161801eff0112ff799e7f01141168881
Gustavo Alcarde <31510006+Chosen-one-Vault13@users.noreply.github.com>
2023-01-16 22:09:40 +0100

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -1,16 +1,3 @@
-bl_info = {
-    "name": "Remove Duplicate Materials",
-    "author": "Gustavo Alcarde-AI",
-    "version": (1, 0, 0),
-    "blender": (2, 83, 0),
-    "location": "View3D > Toolbar > Remove Duplicate Materials",
-    "description": "Removes duplicate materials and remaps them to a single one",
-    "warning": "",
-    "wiki_url": "",
-    "tracker_url": "",
-    "category": "Material"
-}
-
 def register():
     bpy.utils.register_class(RemoveDuplicateMaterials)
 
