652d20946bfc94323d40dee3a00351e5e8d0caa1
Gustavo Alcarde <31510006+Chosen-one-Vault13@users.noreply.github.com>
2023-01-16 23:15:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+from .main_script import RemoveDuplicateMaterialsButton, RemoveDuplicateMaterials, register, unregister
+
 bl_info = {
     "name": "Remove Duplicate Materials",
     "author": "Gustavo Alcarde - AI",
@@ -7,3 +9,14 @@ bl_info = {
     "description": "Removes duplicate materials and remaps them to a single one",
     "category": "Material",
 }
+
+def register():
+    bpy.utils.register_class(RemoveDuplicateMaterialsButton)
+    bpy.utils.register_class(RemoveDuplicateMaterials)
+
+def unregister():
+    bpy.utils.unregister_class(RemoveDuplicateMaterialsButton)
+    bpy.utils.unregister_class(RemoveDuplicateMaterials)
+
+if __name__ == "__main__":
+    register()
